923cfeb89860ced17ea9e9c6cb87c3870c3d4f07
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-02-23 01:09:03 +0200

    up

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
-  return <div>CAR RENTAL</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/catalog" element={<div>Catalog page</div>} />
+        <Route path="/favorites" element={<div>Favorites page</div>} />
+      </Routes>
+    </div>
+  );
 };
