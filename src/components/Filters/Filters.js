486b0199276196da56dd8ce0b876260134585403
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-02-25 23:49:07 +0200

    fix

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,32 +1,19 @@
 import makes from 'data/makes.json';
-import { useSearchParams } from 'react-router-dom';
 import {
   BrandSelect,
   ButtonTitle,
   FiltersContainer,
   SearchButton,
 } from './Filters.styled';
-import { useFormik } from 'formik';
 
-// export const Filters = ({ filters: { brand, price, mileage } }) => {
 export const Filters = () => {
-  // const [params, setParams] = useSearchParams();
-  // const brand = params.get('brand') ?? '';
-  // const price = params.get('price') ?? '';
-  // const changeBrand = evt => {
-  // params.set('brand', evt.target.value);
-  //   setParams(params);
-  // };
-
-  const options = makes.map(make => ({ value: make, label: make }));
-
   return (
     <FiltersContainer>
-      {/* <BrandSelect value={''}>
+      <BrandSelect value={''}>
         {makes.map((make, index) => (
           <option key={index}>{make}</option>
         ))}
-      </BrandSelect> */}
+      </BrandSelect>
 
       <input />
       <input />
